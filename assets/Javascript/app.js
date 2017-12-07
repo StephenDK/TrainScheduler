255eaa6f06ff9b065019bc41dc204ec309699903
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2017-12-06 18:40:49 -0800

    Linked the app.js file

---
+++ assets/Javascript/app.js
@@ -0,0 +1,31 @@
+// Section 1:
+// =========================================================
+// Global Variables and Arrays:
+
+
+
+// Section 2: Initialize Firebase:
+// =========================================================
+
+  var config = {
+    apiKey: "AIzaSyD0kyE7ACEWjgSYQ7hZylcldrQC1y3tWZo",
+    authDomain: "trainscheduler-8daf4.firebaseapp.com",
+    databaseURL: "https://trainscheduler-8daf4.firebaseio.com",
+    projectId: "trainscheduler-8daf4",
+    storageBucket: "",
+    messagingSenderId: "328795968331"
+  };
+  firebase.initializeApp(config);
+
+// variable to reference the database
+var database = firebase.database();
+
+
+// Section 3: Functions
+// =========================================================
+
+
+
+
+// Section 4: Main Process
+// =========================================================
