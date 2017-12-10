711070104d3f238bd3e62ef878fd118f925967fa
StephenDK <stephendklein08@gmail.com>
2017-12-09 17:47:49 -0800

    Added onClick event

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -23,7 +23,64 @@ var database = firebase.database();
 
 // Section 3: Functions
 // =========================================================
+	
+// When the submit button is clicked..
+$("#run-Search").on("click", function(event){
+	// Prevent default form submision
+	event.preventDefault();
 
+	// Grab the users form input information
+	var trainName = $("#train-Name-Input").val().trim();
+	var trainDestination = $("#train-Destination-Input").val().trim();
+	var firstTrain = $("#first-Train-Input").val().trim();
+	var trainFrequency = $("#train-Frequency-Input").val().trim();
+
+	// Tests and debugging
+	console.log(trainName);
+	console.log(trainDestination);
+	console.log(firstTrain);
+	console.log(trainFrequency);
+
+	// Temporary JSON object for holding train data
+	var addTrain = {
+		name: trainName,
+		destination: trainDestination,
+		firstTrain: firstTrain,
+		frequency: trainFrequency
+	};
+
+	//  Push the train data to firebase
+	database.ref().push(addTrain);
+
+	// FireBase Tests and Debuggig
+	console.log(addTrain.name);
+	console.log(addTrain.destination);
+	console.log(addTrain.firstTrain);
+	console.log(addTrain.frequency);
+
+	// Alert the user for adding a train
+	alert("Train successfully added");
+
+	// Clear all of the text boxes
+	$("#train-Name-Input").val("");
+	$("#train-Destination-Input").val("");
+	$("#first-Train-Input").val("");
+	$("#train-Frequency-Input").val("");
+
+	
+});
+
+	// Firebase event to add trains to the database
+	database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+		console.log(childSnapshot.val());
+
+		// Store items from firebase into variables
+		var tName = childSnapshot.val().name;
+		var tDestination = childSnapshot.val().destination;
+		var tFrequency = childSnapshot.val().frequency;
+
+	})
 
 
 
